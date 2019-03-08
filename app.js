9a551c01700aa05cc873c541d7f09952d9b7092e
Rakib Uddin <16103030@iubat.edu>
2019-03-08 16:10:33 +0600

    Dependencies Installed

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+// MiddleWare
+app.use(express.json());
+app.use(express.urlencoded({ extended : false}));
+
+
+
 const port = process.env.port || 5000;
 
 app.listen(port , () => {

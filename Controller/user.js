db2cb47ccca503303c0cc8ab27274031d21a7b8d
Rakib Uddin <16103030@iubat.edu>
2019-03-11 22:39:21 +0600

    Adding form validation

--- Controller/user.js
+++ Controller/user.js
@@ -8,12 +8,36 @@ route.get("/login" , (req , res) => {
 })
 
 route.get("/register" , (req , res) => {
-    res.render("register")
+    res.render("register" , {
+        erre : "",
+        errp : "",
+        errn : "",
+    })
 })
 route.post("/register" , (req , res) => {
     user.findOne({email : req.body.email}).then(response => {
         if(response){
             res.send("Already registered")
+        }
+        else if(!req.body.name){
+            res.render("register" , {
+                errn : "Please Fill The Slots"
+            })
+        }
+        else if(!req.body.email){
+            res.render("register" , {
+                erre : "Please Fill The Slots"
+            })
+        }
+        else if(!req.body.password){
+            res.render("register" , {
+                errp : "Please Fill The Slots"
+            })
+        }
+        else if(req.body.password.length < 7){
+            res.render("register" , {
+                errp : "Password much be greater then 8 characters"
+            })
         }
         else if(req.body.password !== req.body.cpassword){
             res.send("Password doesn't match")

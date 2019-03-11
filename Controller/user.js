6c58aad18d87cd3854f06608b7dd7a3e83d0a537
Rakib Uddin <16103030@iubat.edu>
2019-03-11 20:15:43 +0600

    Registering user to DB

--- Controller/user.js
+++ Controller/user.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const user = require("../Model/User")
 const route = express.Router();
 
 
@@ -10,8 +11,33 @@ route.get("/register" , (req , res) => {
     res.render("register")
 })
 route.post("/register" , (req , res) => {
-    console.log(req.body)
-    res.send("Registration Completed")
+    user.findOne({email : req.body.email}).then(response => {
+        if(response){
+            res.send("Already registered")
+        }
+        else if(req.body.password !== req.body.cpassword){
+            res.send("Password doesn't match")
+        }
+        else{    
+            user({
+                name : req.body.name,
+                email : req.body.email,
+                password : req.body.password
+            })
+            .save()
+            .then(res => {
+                console.log("Data Saved ")
+            })
+            .catch(err => {
+                console.log("OPPS!! Error occures " + err)
+            })
+            res.send("Registration Completed")
+        }
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
 })
     
 
